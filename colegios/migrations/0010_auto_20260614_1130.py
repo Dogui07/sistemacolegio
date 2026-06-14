5d30a40cabff400987f9a3e3d0da371746108f1f
douglas gonzalez <dogui07@gmail.com>
2026-06-14 13:01:35 -0400

    Reparar error de sintaxis en migración 0010

--- colegios/migrations/0010_auto_20260614_1130.py
+++ colegios/migrations/0010_auto_20260614_1130.py
@@ -1,18 +1,11 @@
 from django.db import migrations
-from django.core.management import call_command
-
-def cargar_datos(apps, schema_editor):
-    # Esto busca el archivo data.json en la raíz del proyecto
-    # call_command('loaddata', 'data.json')
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        # IMPORTANTE: Aquí debe ir el nombre del archivo de "merge" 
-        # que se creó justo antes de este.
-        ('colegios', '0009_merge_20260614_1130'), 
+        ('colegios', '0009_merge_20260614_1130'), # Asegúrate de que esto coincida con la migración anterior
     ]
 
     operations = [
-        migrations.RunPython(cargar_datos),
+        # Esta migración no hace nada.
     ]
