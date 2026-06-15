cfd0077d976c7d3e2dd2a76b7b364b754ea98f7d
douglas gonzalez <dogui07@gmail.com>
2026-06-15 08:50:35 -0400

    Agregar migración de datos para colegio

--- web/migrations/0002_add_colegio_data.py
+++ web/migrations/0002_add_colegio_data.py
@@ -1,20 +1,21 @@
+
 from django.db import migrations
 
 def crear_colegio_inicial(apps, schema_editor):
     Colegio = apps.get_model('web', 'Colegio') # 'web' es el nombre de tu app, 'Colegio' el modelo
 
     # Verificamos si ya existe para no duplicarlo
-    if not Colegio.objects.filter(slug='colegio-madre-laura').exists():
+    if not Colegio.objects.filter(slug='Colegio-Madre-Laura').exists():
         Colegio.objects.create(
             nombre="Colegio Madre Laura",
-            slug="colegio-madre-laura",
+            slug="Colegio-Madre-Laura",
             # Puedes agregar otros campos aquí si los necesitas
         )
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        
+        ('web', '0001_initial'),
     ]
 
     operations = [
